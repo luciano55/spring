1ed88b82ac1b7617f542c557350641d172c6e035
Luciano Romero <luciano@bme.es>
2021-02-02 21:15:48 +0100

    Cache endpage nextpage

--- MGXBBDD25 MySql storeProduct cache cache I/src/main/java/com/example/demo/StoreproductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.model.Productstore;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreproductRepository extends JpaRepository<Productstore, Long> {
-
-}
